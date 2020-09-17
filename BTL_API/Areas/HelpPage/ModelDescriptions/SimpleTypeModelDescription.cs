3cf6dfec2dd5b220b756993245bdbebf4a13dcb7
lucquach23 <hoangluc1002@gmail.com>
2020-09-17 15:40:28 +0700

    Add project files.

---
+++ BTL_API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BTL_API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BTL_API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BTL_API.WebApiApplication" Language="C#" %>
